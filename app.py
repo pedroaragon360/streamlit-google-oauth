db806d63fe242080bb12f39ba2c56eaf08e81369
pedroaragon360 <pedroaragon360@gmail.com>
2023-11-15 23:23:50 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,40 +4,57 @@ from google.oauth2.credentials import Credentials
 import requests
 import streamlit.components.v1 as components
 
-# Define the OAuth2 scopes and initialize the flow object
-# ... [your existing init_flow function] ...
+# Define the OAuth2 scopes
+SCOPES = ['https://www.googleapis.com/auth/userinfo.email', 'https://www.googleapis.com/auth/userinfo.profile']
 
-# JavaScript to open a new window for Google OAuth2
-def open_auth_window(auth_url):
-    js = f"""
-    <script>
-    var myWindow = window.open('{auth_url}', 'Google Auth', 'width=600,height=700');
-    // Listen for message from the callback page
-    window.addEventListener('message', (event) => {{
-        if (event.data.type === 'authCode') {{
-            // Handle the received auth code
-            var authCode = event.data.code;
-            // Use Streamlit's methods to handle auth code
-            // ... [handle the auth code in Streamlit] ...
-        }}
-    }}, false);
-    </script>
-    """
-    components.html(js, height=0)
+# Function to initialize the flow object with client ID and secret from Streamlit secrets
+def init_flow():
+    return Flow.from_client_config(
+        client_config={
+            "web": {
+                "client_id": st.secrets['GOOGLE_CLIENT_ID'],
+                "client_secret": st.secrets['GOOGLE_CLIENT_SECRET'],
+                "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+                "token_uri": "https://oauth2.googleapis.com/token",
+                "redirect_uris": [st.secrets['REDIRECT_URI']],
+                "scopes": SCOPES
+            }
+        },
+        scopes=SCOPES,
+        redirect_uri=st.secrets['REDIRECT_URI']
+    )
 
 # Streamlit app layout
 def main():
-    # ... [rest of your Streamlit code, including the OAuth flow initialization] ...
+    flow = init_flow()  # Initialize the flow object here
+
+    # Check for the code parameter in the URL query parameters
+    query_params = st.experimental_get_query_params()
+    if 'code' in query_params:
+        # Complete the authentication process
+        flow.fetch_token(code=query_params['code'][0])
+        credentials = flow.credentials
+        st.session_state['credentials'] = credentials.to_json()
 
     if 'credentials' not in st.session_state:
         # Display login screen
         st.title("Login with Google")
-        auth_url, _ = flow.authorization_url(prompt='consent')
-        open_auth_window(auth_url)
-
-    # ... [rest of your Streamlit app code] ...
+        auth_url, _ = flow.authorization_url(prompt='consent')  # Use the flow object
+        st.markdown(f'[Login]({auth_url})', unsafe_allow_html=True)
+    else:
+        # User is authenticated, show the content screen
+        st.title("Welcome to the App")
+        credentials = Credentials.from_authorized_user_info(st.session_state['credentials'])
+        
+        # Make a request to Google's user info endpoint
+        userinfo_response = requests.get(
+            'https://www.googleapis.com/oauth2/v3/userinfo',
+            headers={'Authorization': f'Bearer {credentials.token}'}
+        )
+        
+        user_info = userinfo_response.json()
+        st.write(f"User info: {user_info}")
 
 # Run the app
 if __name__ == "__main__":
     main()
-

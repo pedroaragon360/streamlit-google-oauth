e452710e0395992afbec3370484ebc89533cc740
pedroaragon360 <pedroaragon360@gmail.com>
2023-11-15 23:30:49 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,10 +2,13 @@ import streamlit as st
 from google_auth_oauthlib.flow import Flow
 from google.oauth2.credentials import Credentials
 import requests
-import streamlit.components.v1 as components
 
-# Define the OAuth2 scopes
-SCOPES = ['https://www.googleapis.com/auth/userinfo.email', 'https://www.googleapis.com/auth/userinfo.profile']
+# Define the OAuth2 scopes (including 'openid')
+SCOPES = [
+    'https://www.googleapis.com/auth/userinfo.email',
+    'https://www.googleapis.com/auth/userinfo.profile',
+    'openid'  # Include the 'openid' scope
+]
 
 # Function to initialize the flow object with client ID and secret from Streamlit secrets
 def init_flow():
@@ -24,43 +27,34 @@ def init_flow():
         redirect_uri=st.secrets['REDIRECT_URI']
     )
 
-# JavaScript to listen for the auth code
-def listen_for_auth_code():
-    js = """
-    <script>
-    window.addEventListener('message', (event) => {
-        if (event.data.type === 'authCode') {
-            const authCode = event.data.code;
-            // Send auth code to Streamlit's server
-            // ... [handle the auth code in Streamlit] ...
-        }
-    }, false);
-    </script>
-    """
-    components.html(js, height=0)
-
 # Streamlit app layout
 def main():
     flow = init_flow()
-    listen_for_auth_code()
+
+    # Check for the code parameter in the URL query parameters
+    query_params = st.experimental_get_query_params()
+    if 'code' in query_params:
+        # Complete the authentication process
+        flow.fetch_token(code=query_params['code'][0])
+        credentials = flow.credentials
+        st.session_state['credentials'] = credentials.to_json()
 
     if 'credentials' not in st.session_state:
         # Display login screen
         st.title("Login with Google")
         auth_url, _ = flow.authorization_url(prompt='consent')
-        st.markdown(f'<a href="{auth_url}" target="_blank">Login</a>', unsafe_allow_html=True)
-
-    if 'credentials' in st.session_state:
+        st.markdown(f'[Login]({auth_url})', unsafe_allow_html=True)
+    else:
         # User is authenticated, show the content screen
         st.title("Welcome to the App")
         credentials = Credentials.from_authorized_user_info(st.session_state['credentials'])
-
+        
         # Make a request to Google's user info endpoint
         userinfo_response = requests.get(
             'https://www.googleapis.com/oauth2/v3/userinfo',
             headers={'Authorization': f'Bearer {credentials.token}'}
         )
-
+        
         user_info = userinfo_response.json()
         st.write(f"User info: {user_info}")
 

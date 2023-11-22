26b498e697c14b5362939765e041fa987c3661d2
Pedro <pedroaragon360@gmail.com>
2023-11-22 12:46:08 +0100

    resultado

--- app.py
+++ app.py
@@ -228,8 +228,8 @@ elif hasattr(st.session_state.run, 'status') and st.session_state.run.status == 
                                 if hasattr(steps.step_details, 'tool_calls'):
                                     with st.expander("Código generado por Code Interpreter"):
                                         st.code(steps.step_details.tool_calls[0].code_interpreter.input)
+                                    with st.expander("Resultado"):
                                         st.markdown(steps.step_details.tool_calls[0].code_interpreter.outputs[0].logs.replace(r'\n', '').replace('\n', '<br>'), unsafe_allow_html=True) #
-                                        st.write(steps.step_details.tool_calls[0].code_interpreter.outputs[0].logs, unsafe_allow_html=False)
                                     
                     #if steps.tools[0].type == 'code_interpreter':
                         # Handle text content

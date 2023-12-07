72d975f1ec73a9b3934d6dcc020feff0b156d05d
Pedro <pedroaragon360@gmail.com>
2023-12-07 17:58:40 +0100

    prog3

--- app.py
+++ app.py
@@ -471,7 +471,8 @@ if hasattr(st.session_state.run, 'status'):
                     message = client.beta.threads.messages.retrieve(message_id = messageid, thread_id=st.session_state.thread.id )
                     for content_part in message.content:
                         if hasattr(content_part, 'text'):
-                            st.write(content_part.text.value)
+                            with st.chat_message('assistant'):
+                                st.write(content_part.text.value)
                     st.toast("MSG recibido y guardado")
                     st.session_state.messages_progress.append(messageid)
                 
